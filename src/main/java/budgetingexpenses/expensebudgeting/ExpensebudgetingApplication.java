7df93bda6c3afc3b6ecd01c1d1e4613905e98ec0
nicholas <nickbr95@gmail.com>
2024-11-07 13:59:25 -0500

    First Commit

---
+++ src/main/java/budgetingexpenses/expensebudgeting/ExpensebudgetingApplication.java
@@ -0,0 +1,13 @@
+package budgetingexpenses.expensebudgeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensebudgetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensebudgetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/budgetingexpenses/expensebudgeting/ExpensebudgetingApplicationTests.java
@@ -0,0 +1,13 @@
+package budgetingexpenses.expensebudgeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensebudgetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
